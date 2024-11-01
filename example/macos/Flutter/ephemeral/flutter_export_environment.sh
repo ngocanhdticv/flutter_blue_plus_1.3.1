baf4135a516712afebbe206211d9058098470852
ngocanhdticv <ngocanhdt.icv@gmail.com>
2024-11-01 17:06:19 +0700

    1.4.0

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -3,7 +3,6 @@
 export "FLUTTER_ROOT=E:\flutter"
 export "FLUTTER_APPLICATION_PATH=E:\code\flutter_blue_plus\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
