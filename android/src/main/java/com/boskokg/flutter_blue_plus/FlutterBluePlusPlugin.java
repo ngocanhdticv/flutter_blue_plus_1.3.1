bfe0d8c07693ca5d6a53742e26662b53e63c3b41
ngocanhdticv <ngocanhdt.icv@gmail.com>
2024-11-02 10:51:37 +0700

    no message

--- android/src/main/java/com/boskokg/flutter_blue_plus/FlutterBluePlusPlugin.java
+++ android/src/main/java/com/boskokg/flutter_blue_plus/FlutterBluePlusPlugin.java
@@ -257,13 +257,13 @@ public class FlutterBluePlusPlugin implements FlutterPlugin, MethodCallHandler, 
       {
         ensurePermissionBeforeAction(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ? Manifest.permission.BLUETOOTH_SCAN : Manifest.permission.ACCESS_FINE_LOCATION, (grantedScan, permissionScan) -> {
           if (grantedScan) {
-            ensurePermissionBeforeAction(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ? Manifest.permission.BLUETOOTH_CONNECT : null, (grantedConnect, permissionConnect) -> {
-              if (grantedConnect)
+//            ensurePermissionBeforeAction(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ? Manifest.permission.BLUETOOTH_CONNECT : null, (grantedConnect, permissionConnect) -> {
+//              if (grantedConnect)
                 startScan(call, result);
-              else
-                result.error(
-                        "no_permissions", String.format("flutter_blue plugin requires %s for scanning", permissionConnect), null);
-            });
+//              else
+//                result.error(
+//                        "no_permissions", String.format("flutter_blue plugin requires %s for scanning", permissionConnect), null);
+//            });
           }
           else
             result.error(
